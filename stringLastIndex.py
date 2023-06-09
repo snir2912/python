f75a407549616e32a0816c0edbf8682051d3345d
snir2912 <89028623+snir2912@users.noreply.github.com>
2023-06-09 18:43:05 +0300

    find the last char in the string

---
+++ stringLastIndex.py
@@ -0,0 +1,5 @@
+print("YO YO YO lets find the last char of the string")
+print("###############################################")
+str = input("please input a string\n")
+last_char = len(str)-1
+print("the last char is: " + str[last_char])
