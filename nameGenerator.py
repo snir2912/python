aba525b8601be742fb2aa41f2ce6b591f138d5c1
snir2912 <89028623+snir2912@users.noreply.github.com>
2023-06-09 18:04:52 +0300

    first commit

---
+++ nameGenerator.py
@@ -0,0 +1,5 @@
+user_name = input("please insert your user name ")
+user_age = input("please insert your user age ")
+user_city = input("please insert your user city ")
+
+print("thank you " + user_name + " your age is: " + user_age + ". yoursn adress is:  " + user_city)
