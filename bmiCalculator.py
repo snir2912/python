9650567a3079ce58dcab60feee18232136f2d9d1
snir2912 <89028623+snir2912@users.noreply.github.com>
2023-06-10 15:09:20 +0300

    print variable in "{}"

--- bmiCalculator.py
+++ bmiCalculator.py
@@ -2,4 +2,4 @@ height = float(input("please insert your height in meter units: \n"))
 weight = int(input("please insert your weight in kg units: \n"))
 bmi = weight / (height ** 2)
 
-print("yout BMI is: ", bmi)
+print(f"yout BMI is: {bmi}")
