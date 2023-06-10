d5ee877750de98d774a456e41ceb57efbf7208e4
snir2912 <89028623+snir2912@users.noreply.github.com>
2023-06-10 15:02:44 +0300

    weight in int type

--- bmiCalculator.py
+++ bmiCalculator.py
@@ -1,5 +1,5 @@
 height = float(input("please insert your height in meter units: \n"))
-weight = float(input("please insert your weight in kg units: \n"))
+weight = int(input("please insert your weight in kg units: \n"))
 bmi = weight / (height ** 2)
 
 print("yout BMI is: ", bmi)
