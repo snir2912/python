04ede34f82924b21a0dd810c9a12b9dbd8a68a39
snir2912 <89028623+snir2912@users.noreply.github.com>
2023-06-10 15:58:40 +0300

    odd and even numbers

---
+++ oddEvenNumbers.py
@@ -0,0 +1,8 @@
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+
+even = number % 2 == 0
+if (even):
+    print("This is an even number.")
+else:
+    print("This is an odd number.")  
--- tipCalculator.py
+++ tipCalculator.py
@@ -1,7 +1,11 @@
 print("welcome to tip calcultaor")
 bill = float(input("what is the total bill?\n"))
 people = float(input("how many people split the bill?\n"))
-precentage_tip = float(input("what precentage tip you would like to give? 10, 12 or 15\n"))
-tip = (bill)  * (precentage_tip) / 100 / (people)
+precentage = float(input("what precentage tip you would like to give? 10, 12 or 15\n"))
 
-print("the tip is: ", tip, "NIS")
+precentage_tip = precentage / 100
+total_tip = bill  * precentage_tip
+tip_per_person = total_tip / people
+round_tip = round(tip_per_person, 2)
+
+print(f"Each person should give {round_tip}NIS for tip")
