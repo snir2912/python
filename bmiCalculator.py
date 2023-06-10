9b9ab554149456ac233f00262a0701c37291a010
snir2912 <89028623+snir2912@users.noreply.github.com>
2023-06-10 14:40:59 +0300

    BMI calculator function

---
+++ bmiCalculator.py
@@ -0,0 +1,5 @@
+height = float(input("please insert your height in meter units: \n"))
+weight = float(input("please insert your weight in kg units: \n"))
+bmi = weight / (height ** 2)
+
+print("yout BMI is: ", bmi)
