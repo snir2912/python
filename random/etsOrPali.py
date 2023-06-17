e7eef03e375ce6e0c3d037dd5e4190225228d4b9
snir2912 <89028623+snir2912@users.noreply.github.com>
2023-06-17 16:20:40 +0300

    two side of coin.

---
+++ random/etsOrPali.py
@@ -0,0 +1,8 @@
+import random
+
+random_number = random.randint(1,2)
+
+if random_number == 1:
+    print("Etz")
+elif random_number == 2:
+    print("Pali")  
---
+++ random/random_numbers.py
@@ -0,0 +1,10 @@
+# radnom int
+import random
+
+a = random.randint(1,6)
+print(a)
+
+# random float number
+
+b = random.random()*5
+print(b)
