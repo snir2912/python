14119ebc752a425db0f173e2193efdabc9df42de
snir2912 <89028623+snir2912@users.noreply.github.com>
2023-06-17 16:47:00 +0300

    pick random index from list

---
+++ random/randomIndexFromList.py
@@ -0,0 +1,9 @@
+import random
+
+lst = ['Delaware', 'Pennsylvania', 'New Jersey', 'Georgia', 'Connecticut', 'Massachusetts', 'Maryland', 'South Carolina', 'New Hampshire', 'Virginia', 'New York', 'North Carolina', 'Rhode Island', 'Vermont', 'Kentucky', 'Tennessee', 'Ohio', 'Louisiana', 'Indiana', 'Mississippi', 'Illinois', 'Alabama', 'Maine', 'Missouri', 'Arkansas', 'Michigan', 'Florida', 'Texas', 'Iowa', 'Wisconsin', 'California', 'Minnesota', 'Oregon', 'Kansas', 'West Virginia', 'Nevada', 'Nebraska', 'Colorado', 'North Dakota', 'South Dakota', 'Montana', 'Washington', 'Idaho', 'Wyoming', 'Utah', 'Oklahoma', 'New Mexico', 'Arizona', 'Alaska', 'Hawaii']
+
+lengthList = len(lst)
+# print(lengthList)
+countryIndex = random.randint(0, lengthList)
+
+print(lst[countryIndex])
